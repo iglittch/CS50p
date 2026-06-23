2f7d20b98d576bfc8ad36fae17073728b5d6bd4f
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-06-24 00:54:00 +0300

    Update indoor.py
    Implemented custom functions

--- Week 0/indoor.py
+++ Week 0/indoor.py
@@ -1,7 +1,12 @@
-#Prompt the user input
-voice = input()
+def main():
+    #Prompt the user input
+    voice = input()
+    voice = volumeDown(voice)
+    print(voice)
 
 
-#Output the same input in lowercase (no change to whitespace and punctuation)
-volume = voice.lower()
-print(volume)
+def volumeDown(tone):
+    #Output the same input in lowercase
+    return tone.lower()
+
+main()
