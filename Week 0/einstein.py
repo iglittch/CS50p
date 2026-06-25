e2b3d7484d92005064b9eb781b5a43beb84c334c
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-06-25 21:47:52 +0300

    Update einstein.py

--- Week 0/einstein.py
+++ Week 0/einstein.py
@@ -1,5 +1,23 @@
 # Prompt user for mass
-
 # Convert input to int
+def main():
+    mass = int(input("What is the mass?"))
+    answer = equation(mass)
+
+    print(f"Joules:{answer}")
+
 
 # Calculate Joules
+def equation(m):
+    # Equation : e = mc2
+    c = 300000000 
+    e = m * pow(c,2)
+
+    return e
+
+main()
+
+
+
+    
+
