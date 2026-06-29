f5f00139818977716afa2392d6664e40c70c50cb
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-06-30 00:08:37 +0300

    Update deep.py

--- Week 1/deep.py
+++ Week 1/deep.py
@@ -4,13 +4,7 @@ def main():
 # Take care of case sensitivity
     answer = answer.casefold()
 # If answer is 42 print yes
-    if answer == "42":
-        print("Yes")
-    elif answer == "forty two":
-        print("Yes")
-    elif answer == "forty-two":
-        print("Yes")
-    elif answer == "fortytwo":
+    if answer == "42" or answer == "forty two" or answer == "forty-two" or answer == "fortytwo":
         print("Yes")
     else:
         print("No")
