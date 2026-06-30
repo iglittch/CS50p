f3595c8277f20976e93c8a46501dcb46e681fed4
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-07-01 01:03:32 +0300

    Update interpreter.py

--- Week 1/interpreter.py
+++ Week 1/interpreter.py
@@ -1,17 +1,18 @@
 import operator
 def main():
-# Prompt for the expressions
+    # Prompt for the expressions
     expression = input("Calculate: ")
-    # Split spaces btwn x y z
+    # Split spaces btwn x y z assuming the spaces
     x,y,z = expression.split(" ")
     x = int(x)
     z = int(z)
     y = convert_operator(y)
 
+    # Formulate calculations
+    # Output numbers correct to 1 decimal place
     print(f"{y(x,z):.1f}")
 
-# Formulate calculations
-# Output numbers correct to 1 decimal place
+
 def convert_operator(op):
     match op:
         case "+":
