baad8e8638a56e59d84fe7b7fcd99c7a7e99c462
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-07-01 17:35:49 +0300

    Create meal.py

---
+++ Week 1/meal.py
@@ -0,0 +1,29 @@
+# Prompt user for a time
+# Output whether its breakfast, lunch or dinner time
+# Default dont print anything
+# Assume format will be 24-hour time ##:##
+# Assume each meal time range is inclusive
+
+def main():
+    schedule = input("What time is it? ")
+    schedule = convert(schedule)
+
+    if 7.00 <= schedule <= 8.00:
+        print("Breakfast time")
+    elif 12.00 <= schedule <= 13.00:
+        print("Lunch time")
+    elif 18.00 <= schedule <= 19.00:
+        print("Dinner time")
+    else:
+        print(" ")
+
+
+def convert(time):
+    time = time.replace(":",".")
+    time = float(time)
+    
+    return time
+
+
+if __name__ == "__main__":
+    main()
