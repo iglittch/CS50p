5f8e4bc6a489cc6e4d5709e294b23aee4c3adf4d
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-07-04 23:55:19 +0300

    Update camel.py

--- Week 2/camel.py
+++ Week 2/camel.py
@@ -1,5 +1,23 @@
 # Prompt user for variable name in camelcase
-# Assume user input will be in camelcase
+def main():
+    variable_name = input("camelCase: ")
 
-# Output the input name to snake case
+    # Assume user input will be in camelcase
+    modified_name = convert_to_snake(variable_name)
+    # Output the input name to snake case
+    print(f"Snake Case: {modified_name}")
 
+def convert_to_snake(name):
+    char_list = []
+
+    for i,char in enumerate(name):
+        if char.isupper() and i != 0:
+            char_list.append("_")
+        char_list.append(char)
+
+    new_name = "".join(char_list).lower()
+    return new_name
+
+    
+
+main()
