43ef4901a9729f14aaca146855b3011328d75a95
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-07-05 00:55:38 +0300

    Create coke.py

---
+++ Week 2/coke.py
@@ -0,0 +1,21 @@
+# Show coke bottle price
+coke_price = 50
+print(f"Amount Due: {coke_price}")
+
+# Prompt user to insert a coin amount at a time
+coin_paid = int(input("Insert Coin: "))
+
+amount_due = coke_price - coin_paid
+change = coin_paid - coke_price
+
+if coin_paid in [25,10,5]:
+    while amount_due != 0 and amount_due > 0:
+        print(f"Amount Due: {amount_due}")
+        coin_paid = int(input("Insert Coin: "))
+        amount_due -= coin_paid
+
+    if change >= 0:
+        print(f"Change: {change}")
+else:
+    print("Invalid Coin")
+    
