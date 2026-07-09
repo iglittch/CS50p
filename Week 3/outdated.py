2f4d50d07221e0cb53729b857ae043ec070487c7
Sophia Jama <72500901+iglittch@users.noreply.github.com>
2026-07-10 01:05:33 +0300

    Exception Handing update

--- Week 3/outdated.py
+++ Week 3/outdated.py
@@ -4,6 +4,9 @@ def main():
 
     print(new_date)
 
+class DayMonthYearInputError(Exception):
+    pass
+
 def modify_format(date):
     months = [
     "January",
@@ -19,24 +22,45 @@ def modify_format(date):
     "November",
     "December"
 ]
-    for m in months:
-        if m in date:
-            mm,dd,yyyy = date.split()
+    while True:
+        try:
+            if "/" not in date:
+                mm,dd,yyyy = date.split()
 
-            dd = dd.replace(",","")
-            mm = months.index(mm) + 1
+                dd = dd.replace(",","")
+                mm = months.index(mm) + 1
+                    
+                dd = int(dd)
+                if dd > 31 or dd < 1:
+                    raise DayMonthYearInputError(dd)
+                elif len(yyyy) > 4:
+                    raise DayMonthYearInputError(yyyy)
+                
+                new_format = f"{yyyy}-{mm:02d}-{dd:02d}"
+
+                return new_format
+
+            else:
+                mm,dd,yyyy = date.split("/")
+                dd = int(dd)
+                mm = int(mm)
+
+                
+                if mm > 12 or mm < 1:
+                    raise DayMonthYearInputError(mm)
+                elif dd > 31 or dd < 1:
+                    raise DayMonthYearInputError(dd)
+                elif len(yyyy) > 4:
+                    raise DayMonthYearInputError(yyyy)
+                else:
+                    new_format = f"{yyyy}-{mm:02d}-{dd:02d}"
+
+                return new_format
             
-            new_format = f"{yyyy}-{mm}-{dd}"
-
-            return new_format
-
-        else:
-            mm,dd,yyyy = date.split("/")
-
-            new_format = f"{yyyy}-{mm}-{dd}"
-
-            return new_format
-            
+        except (DayMonthYearInputError,ValueError) as error:
+            date = input("Date: ")
+        
 
 
 main()
+
